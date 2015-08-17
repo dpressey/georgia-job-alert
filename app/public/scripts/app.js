abf00f0349945aebf807a5c6558b21dfd4d93bc0
Denzel Pressey <pressey90@gmail.com>
2015-08-16 22:49:33 -0400

    added controller and job search view

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -2,7 +2,14 @@ var app = angular.module('job-alert', ['ngRoute', 'ui.bootstrap']);
 
 app.config(['$routeProvider','$locationProvider',
 	function($routeProvider, $locationProvider) {
-		
+		$routeProvider.
+			when('/', {
+				templateUrl: 'views/job_search.html',
+				controller: 'JobSearchController'
+			}).
+			otherwise({
+				redirectTo: '/'
+			});
 
 		// Use the HTML5 History API
 		//$locationProvider.html5Mode(true);		
---
+++ app/public/scripts/controllers/controllers.js
@@ -0,0 +1,3 @@
+app.controller('JobSearchController', function ($scope){
+	
+});
