9bc4c0db0564c700e3a4901eca26bfa2703ef6eb
Denzel Pressey <pressey90@gmail.com>
2015-08-26 20:36:19 -0400

    added validation directives and added service module

--- app/public/scripts/controllers/controllers.js
+++ app/public/scripts/controllers/controllers.js
@@ -1,3 +1,16 @@
-app.controller('JobSearchController', function ($scope){
-	
+app.controller('JobSearchController', function ($scope, jobService){
+		
+		$scope.form = {
+			city: '',
+			desiredPosition: '',
+			expectedExperience: '',
+			expectedEducation: ''
+		};	
+
+		$scope.searchForJobs = function(isValid){
+
+			if (isValid){
+				jobService.getJobs( $scope.form );
+			}
+		};	
 });
---
+++ app/public/scripts/services/service.js
@@ -0,0 +1,11 @@
+app.service('jobService', function($http){
+
+	function getJobs( formData ){
+		return true;
+	}
+
+	return {
+		getJobs: getJobs
+	}
+
+});
